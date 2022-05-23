c5c1d319e5938e19ff4b13b721a4c5fced5e08df
Navika12 <105738173+Navika12@users.noreply.github.com>
2022-05-23 00:14:56 -0500

    adding code for buttons

--- Rogue Lite Game/Assets/MainMenu.cs
+++ Rogue Lite Game/Assets/MainMenu.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.ScreenManagement;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -12,13 +12,13 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void StartGame()
