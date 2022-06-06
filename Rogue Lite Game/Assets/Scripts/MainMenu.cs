2aeefe695849496e3ff3e1ed5321cbebd602fc6a
Navika12 <105738173+Navika12@users.noreply.github.com>
2022-06-06 12:16:51 -0500

    update of credits

--- Rogue Lite Game/Assets/Scripts/MainMenu.cs
+++ Rogue Lite Game/Assets/Scripts/MainMenu.cs
@@ -7,18 +7,6 @@ public class MainMenu : MonoBehaviour
 { 
     public GameObject optionsScreen, creditsScreen;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void StartGame()
     {
         SceneManager.LoadScene("Main Game", LoadSceneMode.Single);
